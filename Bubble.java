311fd294252798e1e916ecda43c00202cd46e5e2
royalai004 <156927439+royalai004@users.noreply.github.com>
2024-02-28 23:34:06 -0500

    Update Bubble.java

--- Bubble.java
+++ Bubble.java
@@ -1,12 +1,46 @@
-// Read the README.md
-// Read it again
-// Your code here
-// Delete these 4 lines of comments
+import java.util.Scanner;
 
 class B {
-    public static void main(String[] args) {
+    // Sorts array using bubble sort algorithm
+    static void bubbleSort(int[] arr, int n) {
+        int i, j, temp;
+        boolean swapped;
+        for (i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            printArray(arr, n);
+            if (!swapped)
+                break;
+        }
+    }
 
-        System.out.println("Let's Bubble it up!");
-        
+    // Prints array elements
+    static void printArray(int[] arr, int size) {
+        for (int i = 0; i < size; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        // Input array size
+        System.out.println("Enter array size");
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] arr = new int[n];
+        // Input array elements
+        System.out.println("Enter array elements");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scan.nextInt();
+        }
+        bubbleSort(arr, n);
+        System.out.println("Sorted array");
+        printArray(arr, n);
     }
 }
